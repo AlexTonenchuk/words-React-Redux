7557e4e2ec9c6e0475b934be0a742efa85f5df6f
Alex Tonenchuk <25725at@gmail.com>
2023-01-25 21:37:27 +0300

    Updates

---
+++ precache-manifest.ba047f9d3cdbf5175bb1b652799c5d8d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f17116fc74857c20bc0e",
+    "url": "/words-React-Redux/static/css/main.3ebc6187.chunk.css"
+  },
+  {
+    "revision": "f17116fc74857c20bc0e",
+    "url": "/words-React-Redux/static/js/main.054c20ea.chunk.js"
+  },
+  {
+    "revision": "693a44557375b3a34c43",
+    "url": "/words-React-Redux/static/js/runtime~main.41c17428.js"
+  },
+  {
+    "revision": "f4226e16e8cd20bc86ec",
+    "url": "/words-React-Redux/static/js/2.1cba8f3c.chunk.js"
+  },
+  {
+    "revision": "dc5903090a4da9c333be22f7126e6277",
+    "url": "/words-React-Redux/index.html"
+  }
+];
